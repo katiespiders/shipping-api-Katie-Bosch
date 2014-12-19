58d292a0cadde5bb618b391466ca7ce390f39327
Katie <kab761@gmail.com>
2014-12-19 10:47:00 -0800

    cleanup

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,3 @@
-
 require 'active_shipping'
 include ActiveMerchant::Shipping
 
@@ -11,55 +10,49 @@ class ShipmentsController < ApplicationController
     end
 
     rates
-    render json: @rates
+    render json: @rates, status: 202
   end
 
   private
 
-  def fedex
-    @carrier_obj = FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PW'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCT'], test: true)
-  end
+    def fedex
+      @carrier_obj = FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PW'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCT'], test: true)
+    end
 
-  def usps
-    @carrier_obj = USPS.new(login: ENV['USPS_KEY'])
-  end
+    def usps
+      @carrier_obj = USPS.new(login: ENV['USPS_KEY'])
+    end
 
-  def rates
-    response = @carrier_obj.find_rates(origin, destination, packages)
-    @rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-  end
+    def rates
+      response = @carrier_obj.find_rates(origin, destination, packages)
+      @rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      raise
+    end
 
-  def carrier
-    # params[:carrier]
-    'FedEx'
-  end
+    def carrier
+      params[:carrier]
+    end
 
-  def packages
-    # params[:packages]
-    [
-      Package.new(100,        # 100 grams
-      [93,10],                # 93 cm long, 10 cm diameter
-      :cylinder => true),     # cylinders have different volume calculations
+    def packages
+      packages_hash, packages_array = params[:packages], []
+      packages_hash.each do |index, package|
+        weight = package[:weight].to_i
+        dimensions = package[:dimensions].collect {|d| d.to_i}
+        packages_array << Package.new(weight, dimensions)
+      end
+      packages_array
+    end
 
-      Package.new((7.5 * 16), # 7.5 lbs, times 16 oz/lb.
-      [15, 10, 4.5],          # 15x10x4.5 inches
-      :units => :imperial)    # not grams, not centimetres
-    ]
-  end
+    def origin
+      Location.new( {
+        country: 'US',
+        state: 'WA',
+        city: 'Seattle',
+        zip: '98103'
+      })
+    end
 
-  def origin
-    # params[:origin]
-    Location.new(:country => 'US',
-    :state => 'CA',
-    :city => 'Beverly Hills',
-    :zip => '90210')
-  end
-
-  def destination
-    # params[:destination]
-    Location.new(:country => 'CA',
-    :province => 'ON',
-    :city => 'Ottawa',
-    :postal_code => 'K1P 1J1')
-  end
+    def destination
+      Location.new(params.require(:destination).permit(:country, :state, :city, :zip))
+    end
 end
